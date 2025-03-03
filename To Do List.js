3c8b6f0dbdbfa3cc1ddf8db9b61420c3e2853bfb
prantik2005 <prantiksurai05@gmail.com>
2025-03-04 01:11:53 +0530

    add

--- To Do List.js
+++ To Do List.js
@@ -2,13 +2,22 @@ function create_task(){
     let tasklist = document.getElementById("task-list");
     let taskinput = document.getElementById("taskinput");
     let newtask = document.createElement("li");
-    newtask.textContent = taskinput.value;
+
     let checkbox = document.createElement("input");
     checkbox.type = "checkbox";
+    checkbox.classList.add("checkbox-button");
+
+    let tasktext = document.createElement("span");
+    tasktext.textContent = taskinput.value;
+
     let remove = document.createElement("button");
-    remove.textContent = "Remove";
-    newtask.appendChild(checkbox);
+    remove.classList.add("remove-button");
+    remove.textContent = "REMOVE";
+
+    newtask.append(checkbox);
+    newtask.appendChild(tasktext);
     newtask.appendChild(remove);
+    
     tasklist.appendChild(newtask);
     taskinput.value = "";
     checkbox.addEventListener("click" , function(){
