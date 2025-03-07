d4e5519aec9592c2a0cc79a5fbb484b917622faf
prantik2005 <prantiksurai05@gmail.com>
2025-03-07 17:24:17 +0530

    add

--- checkbox.js
+++
@@ -1,6 +0,0 @@
-let click = document.getElementById("button");
- click.addEventListener("click", function(){
-    let check = document.createElement("input");
-    check.type = "checkbox";
-    document.body.appendChild(check);
- });
